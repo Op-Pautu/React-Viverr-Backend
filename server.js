679b085b2d7b7f556b09d94c79e587b6ba1da17b
Op Pautu <pautuop@gmail.com>
2023-03-08 13:01:44 +0530

    Add initial files

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+const app = express();
+dotenv.config();
+
+mongoose.set("strictQuery", true);
+
+const connect = async () => {
+  try {
+    mongoose.connect(process.env.MONGO);
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+app.listen(3000, () => {
+  connect();
+  console.log("Server is running on port 3000");
+});
